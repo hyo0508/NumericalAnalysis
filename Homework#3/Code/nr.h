2aaad166fd06759693ce0bf96caf6fb6d67103ca
hyo0508 <2018008168@hanyang.ac.kr>
2022-10-10 21:39:02 +0900

    add: lubksb.c

--- Homework#3/Code/nr.h
+++ Homework#3/Code/nr.h
@@ -4,5 +4,6 @@
 void gaussj(float **a, int n, float **b, int m);
 void ludcmp(float **a, int n, int *indx, float *d);
 void svdcmp(float **a, int m, int n, float w[], float **v);
+void lubksb(float **a, int n, int *indx, float b[]);
 
 #endif
