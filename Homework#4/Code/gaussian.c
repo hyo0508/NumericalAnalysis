c71e3df35be2962267d6590b765000c2309f4fca
hyo0508 <2018008168@hanyang.ac.kr>
2022-11-09 11:27:25 +0900

    Homework#4 Init

---
+++ Homework#4/Code/gaussian.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+#include "nr.h"
+
+int main()
+{
+    printf("gaussian\n");
+}
---
+++ Homework#4/Code/nr.h
@@ -0,0 +1,7 @@
+#ifndef NR_H_
+#define NR_H_
+
+float gasdev(long *idum);
+float ran1(long *idum);
+
+#endif
